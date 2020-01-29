a74e2e3131a8d7b4347b1b0b187dfa0de0daed82
MrBlockCat <37186692+MrBlockCat@users.noreply.github.com>
2020-01-29 16:00:07 -0500

    Add files via upload
    The initial versions of PorjectAlpha (the thing you run to start the game) and StudioAnimations (pretty self explanatory)

---
+++ StudioAnimations.java
@@ -0,0 +1,22 @@
+public class StudioAnimations
+{
+	public static void FalconCoLtd()
+	{
+		//Insert Falcon's studio intro animation here
+	}
+	
+	public static void DigoEntertainment()
+	{
+		//You can name this method whatever you want and show whatever you want in here as long as it doesn't break lol
+	}
+	
+	public static void MattEntertainment()
+	{
+		//You can name this method whatever you want and show whatever you want in here as long as it doesn't break lol
+	}
+	
+	public static void JosephEntertainment()
+	{
+		//You can name this method whatever you want and show whatever you want in here as long as it doesn't break lol
+	}
+}
