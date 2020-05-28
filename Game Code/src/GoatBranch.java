9c15c3f97c0ae48fb50916fa04c5d5447b89073b
Matt F <38508371+frenchfreys@users.noreply.github.com>
2020-05-28 17:21:23 -0400

    story stuffs
    wow

--- Game Code/src/GoatBranch.java
+++ Game Code/src/GoatBranch.java
@@ -21,6 +21,10 @@ public class GoatBranch
 			case "RockOfSelfReflection":
 				RockOfSelfReflection();
 				break;
+			case "5":
+			case "Tree":
+				Tree();
+				break;
 			default:
 				System.out.println("Error 5: The starting area of the goat branch of the story, method GoToGoatBranch, was told to go to " + StartingPosition + ", which isn't a place!");
 		}
@@ -32,8 +36,14 @@ public class GoatBranch
 		
 		if (Stuff.StoryBeginning == true)
 		{
-			Stuff.TypeLine("You travel back to the year 1954.");
-			Stuff.typeln ("And also, for some reason, you now embody a goat.");
+			Stuff.TypeLine("And everything is black.");
+			Stuff.Wait(2000);
+			Stuff.typeln("For a few seconds, at least.");
+			Stuff.typeln("Soon enough, you wake up.");
+			Stuff.typeln("It is with great surprise that you find Doof's machine didn't harm you.");
+			Stuff.typeln ("Well... except that...");
+			Stuff.Wait(1000);
+			Stuff.typeln ("You are a goat.");
 			Stuff.typeln ("That is, despite having the same human (presumably) brain you had before, you now are a goat.");
 			Stuff.typeln ("What fun things shall you do first?");
 			Stuff.StoryBeginning = false;
@@ -60,7 +70,31 @@ public class GoatBranch
 			}
 		}
 		else {
-			Stuff.typeln ("Investigation time, I suppose.");
+			Stuff.typeln ("Exploration time, I suppose.");
+			Stuff.Wait(1500);
+			Stuff.typeln("After some brief exploration, you are able to formulate the following map.");
+			Stuff.typeln("It's quite messy, which is owed to your lack of thumbs or fingers.");
+			ASCII.Map2();
+			Stuff.typeln("Where ever shall you go? (so many fun locations!)");
+			switch (Stuff.PlayerChoice("mountain", "rock", "grocery store", "nowhere"))
+			{
+			case "mountain":
+				Stuff.typeln ("Good choice. You are a goat, after all.");
+				Mountain();
+				break;
+			case "rock":
+				Stuff.typeln ("Yes, perhaps the rock will hold some answers...");
+				RockOfSelfReflection();
+				break;
+			case "grocery store":
+				Stuff.typeln ("As one often is after time travel, you are quite hungry. Luckily, as a goat, you can eat things you couldn't as a human.");
+				GroceryStore();
+				break;
+			case "nowhere":
+				Stuff.typeln ("Turns out the map was a waste of time. You'd like to stay put.");
+				Tree();
+				break;
+			}
 		}
 	}
 	
@@ -68,6 +102,26 @@ public class GoatBranch
 		Stuff.CurrentLocationNumber = 2;
 		
 		Stuff.typeln ("\"Welcome to the mountain\" says a nearby tree.");
+		Stuff.typeln ("\"Hmm,\" you say to yourself, \"I suppose that the trees are sentient here.\"");
+		Stuff.typeln ("Maybe the talking tree will have answers for you. Or maybe you should just begin your climb.");
+		switch (Stuff.PlayerChoice("climb", "discuss with tree"))
+		{
+		case "climb":
+			Stuff.typeln ("Good choice. You are a goat, after all.");
+			break;
+		case "discuss with tree":
+			Stuff.typeln("\"Why are you able to talk?\" you ask the tree.");
+			Stuff.typeln("\"Mate,\" says the tree, \"I have been through the unthinkable.\"");
+			Stuff.typeln("\"You're not going to want to hear it.\" ");
+			Stuff.typeln("\"Yes I am!\" you protest, but alas. The tree has gone back to sleep, leaving you answerless.");
+			Stuff.typeln("Or so you think. Until he drops a leaf.");
+			Stuff.Wait(1000);
+			Stuff.typeln("You examine it carefully. . .  and find that there is text on it!");
+			ASCII.Leaf();
+			Stuff.Wait(5000);
+			Stuff.typeln("Well... either way, time to climb the mountain I suppose.");
+			break;
+		}
 		
 	}
 	
@@ -83,5 +137,11 @@ public class GoatBranch
 		
 		Stuff.typeln("Lucky for you, there was a sick looking rock nearby that should be good for self-reflection.");
 		
+	}
+	
+	public static void Tree() {
+		Stuff.CurrentLocationNumber = 5;
+		
+		Stuff.typeln("A nearby tree provides some shade.");
 	}
 }
