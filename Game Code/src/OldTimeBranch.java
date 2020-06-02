12193d0d66fb00bfc6613421ff522e5928416973
MrBlockCat <37186692+MrBlockCat@users.noreply.github.com>
2020-06-01 20:24:25 -0400

    Some tweaks to the Old Time story branch

--- Game Code/src/OldTimeBranch.java
+++ Game Code/src/OldTimeBranch.java
@@ -10,8 +10,9 @@ public class OldTimeBranch
 				OuterVillage1();
 				break;
 			case "2":
-			case "RouletShop":
-				RouletShop();
+			case "RoulletteShop":
+				RoulletteShop();
+				break;
 			case "3":
 			case "Bazar":
 				Bazar();
@@ -39,14 +40,14 @@ public class OldTimeBranch
 			Stuff.TypeLine("Dr. Heinz Doofenshmirtz mentioned that a couple times, not that you were paying attention.");
 			Stuff.TypeLine("You take the poster off the wall and behind it is a map of the kingdom.");
 			ASCII.OldTimeMap1();
+			Stuff.HitEnter(1);
 			Stuff.Wait(2000);
-			Stuff.TypeLine("");
 			Stuff.TypeLine("\"Where will you begin your search?\" you hear a passer by ask.");
 			switch (Stuff.PlayerChoice("explore the bazar", "check out barracks", "visit the harbor", "search the castle"))
 			{
 				case "explore the bazar":
 					Stuff.StoryBeginning = false;
-					RouletShop();
+					RoulletteShop();
 					break;
 				case "check out the barracks":
 					Stuff.StoryBeginning = false;
@@ -65,50 +66,46 @@ public class OldTimeBranch
 		}	
 	}
 	
-	public static void RouletShop()
+	public static void RoulletteShop()
 	{
 		Stuff.CurrentLocationNumber = 2;
 		
-		Stuff.TypeLine("Whilst perusing the shops, some infinity gauntlet esk glove catches your eye.");
+		Stuff.TypeLine("Whilst perusing the shops, some infinity gauntlet esque glove catches your eye.");
 		Stuff.TypeLine("Engraved in it is three symbols. A rocket ship, the head of a goat, and a sword.");
-		Stuff.TypeLine("\"You like what you're seeing? Even I dont know how it got here or what that turnip shaped thing is\"");
-		Stuff.TypeLine("\"but I found it right outside my farm a couple o' years ago, no clue how long its been there\"");
+		Stuff.TypeLine("\"You like what you're seeing? Even I don't know how it got here or what that turnip shaped thing is\"");
+		Stuff.TypeLine("\" but I found it right outside my farm a couple o' years ago, no clue how long it's been there.\"");
 		Stuff.TypeLine("\"Care to play a little roullette? I spin this wheel, and if the arrow lands on, what say you, the number 5\"");
-		Stuff.TypeLine("\"You get the \"Lucky Gauntlet\", sounds fair? Well is doesnt matter because I make the rules\"");
+		Stuff.TypeLine("\"You get the \"Lucky Gauntlet\", sounds fair? Well it doesn't matter because I make the rules!\"");
 		switch (Stuff.PlayerChoice("spin", "don't spin"))
 		{
 			case "spin":
-				Stuff.RandomInt(1, 6);
 				Stuff.RoulletteSpin = true;
 				
-				if (Stuff.RandomInt() != 5)
+				if (Stuff.RandomInt(1, 6) != 5)
 				{
-					Stuff.TypeLine("\"Oh not your lucky day is it. Well move along now, nothing else you can do here, well besides taking this sword for your troubles\"");
-					Stuff.TypeLine("\" you are gonna need it on your quest for that assasin!\"");
-					Stuff.TypeLine("The sword is made of emerals which gives it a magnificent green glow in the sun.");
-					Stuff.TypeLine("As you marvel at the sword, you walk away to start your search of the assasin.");
-					Stuff.HasLuckyGauntlet = false;
+					Stuff.TypeLine("\"Oh not your lucky day is it. Well move along now, nothing else you can do here, well besides taking this sword for your troubles.\"");
+					Stuff.TypeLine("\"You are gonna need it on your quest for that assassin!\"");
+					Stuff.TypeLine("The sword is made of emeralds which gives it a magnificent green glow in the sun.");
+					Stuff.TypeLine("As you marvel at the sword, you walk away to start your search of the assassin.");
 					Stuff.HasEmeraldSword = true;
 					Bazar();
 				}
 				
 				else
 				{
-					Stuff.TypeLine("\"Arent you lucky! Well a deal is a deal. You best be on your way now, I hear you are chasing down the assasin\"");
-					Stuff.TypeLine("He wishes you good luck and you go on your way. ");
+					Stuff.TypeLine("\"Aren't you lucky! Well a deal is a deal. You best be on your way now, I hear you are chasing down the assassin.\"");
+					Stuff.TypeLine("He wishes you good luck and you go on your way.");
 					Stuff.HasLuckyGauntlet = true;
-					Stuff.HasEmeraldSword = false;
 					Bazar();
 				}
 				break;
 				
-			case "dont spin":
-			{
-				Stuff.TypeLine("\"Well then, here take this sword, its made of emeralds and will help you on your journey.\"");
-				Stuff.TypeLine("\"Most people around here know about it and boy are we thankfull. Our kind is a good one so I dont know why anyone would attempt to murder him.\"");
-				Stuff.TypeLine("\'Good luck travelor.\" With these words you leave the shop and continue your search.");
+			case "don't spin":
+				Stuff.TypeLine("\"Well then, here take this sword, it's made of emeralds and will help you on your journey.\"");
+				Stuff.TypeLine("\"Most people around here know about it and boy are we thankful. Our king is a good one so I don't know why anyone would attempt to murder him.\"");
+				Stuff.TypeLine("\"Good luck traveler.\" With these words you leave the shop and continue your search.");
+				Stuff.HasEmeraldSword = true;
 				Bazar();
-			}
 				break;
 		}
 	}
